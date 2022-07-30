979100fba46db5de5c7a574a53c876584cb32298
unknown <doloresalemang@gmail.com>
2022-07-30 11:25:42 -0300

    first commit

---
+++ src/com/dh/medicamentos/dao/impl/MedicamentoDaoMariaDb.java
@@ -0,0 +1,16 @@
+package com.dh.medicamentos.dao.impl;
+
+import com.dh.medicamentos.dao.IDao;
+import com.dh.medicamentos.model.Medicamento;
+
+public class MedicamentoDaoMariaDb implements IDao<Medicamento> {
+    @Override
+    public Medicamento guardar(Medicamento medicamento) {
+        return null;
+    }
+
+    @Override
+    public Medicamento buscar(Integer id) {
+        return null;
+    }
+}
---
+++ src/com/dh/medicamentos/dao/util/Utils.java
@@ -0,0 +1,5 @@
+package com.dh.medicamentos.dao.util;
+
+public class Utils {
+
+}
